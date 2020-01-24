c57ab1bd19df76bce60803e1de0107c718d4fbaf
MinexAutomation <minex.automation@gmail.com>
2020-01-24 14:07:08 -0800

    Added service collection extensions.

---
+++ source/R5T.Solutas.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Cambridge.Types;
+using R5T.Megara.TextSerializer;
+using R5T.Solutas.Megara;
+using R5T.Solutas.Tiros;
+
+
+namespace R5T.Solutas.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddVisualStudioSolutionFileSerizlier(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IVisualStudioSolutionFileSerializer, VisualStudioSolutionFileSerializer>()
+                .AddTextFileSerializer<SolutionFile, VisualStudioSolutionFileTextSerializer>()
+                ;
+
+            return services;
+        }
+    }
+}
