aa88802a941d53f3b8ca25c35ab2dba317c52001
MinexAutomation <minex.automation@gmail.com>
2020-01-24 16:44:21 -0800

    Added startup and program-as-service.

--- source/R5T.Solutas.Standard.Construction/Code/Program.cs
+++ source/R5T.Solutas.Standard.Construction/Code/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Chalandri;
+using R5T.Evosmos;
+using R5T.Ilioupoli.Default;
+using R5T.Richmond;
+
+
 namespace R5T.Solutas.Standard.Construction
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceProvider = ServiceProviderBuilder.NewUseStartup<Startup>();
+
+            var program = serviceProvider.GetRequiredService<Program>();
+
+            program.Run();
+        }
+
+
+        private ITestingDataDirectoryContentPathsProvider TestingDataDirectoryContentPathsProvider { get; }
+        private ITemporaryDirectoryFilePathProvider TemporaryDirectoryFilePathProvider { get; }
+        private IVisualStudioSolutionFileSerializer VisualStudioSolutionFileSerializer { get; }
+
+
+        public Program(
+            ITestingDataDirectoryContentPathsProvider testingDataDirectoryContentPathsProvider,
+            ITemporaryDirectoryFilePathProvider temporaryDirectoryFilePathProvider,
+            IVisualStudioSolutionFileSerializer visualStudioSolutionFileSerializer)
+        {
+            this.TestingDataDirectoryContentPathsProvider = testingDataDirectoryContentPathsProvider;
+            this.TemporaryDirectoryFilePathProvider = temporaryDirectoryFilePathProvider;
+            this.VisualStudioSolutionFileSerializer = visualStudioSolutionFileSerializer;
+        }
+
+        public void Run()
+        {
+            var exampleSolutionFilePath = this.TestingDataDirectoryContentPathsProvider.GetExampleVisualStudioSolutionFilePath();
+
+            var solutionFile = this.VisualStudioSolutionFileSerializer.Deserialize(exampleSolutionFilePath);
+
+            var fileName = TestingDataDirectoryContentConventions.ExampleVisualStudioSolutionFileNameValue;
+            var outputSolutionFilePath = this.TemporaryDirectoryFilePathProvider.GetTemporaryDirectoryFilePath(fileName);
+
+            this.VisualStudioSolutionFileSerializer.Serialize(outputSolutionFilePath, solutionFile);
         }
     }
 }
---
+++ source/R5T.Solutas.Standard.Construction/Code/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using R5T.Chalandri.DropboxRivetTestingData;
+using R5T.Evosmos.CDriveTemp;
+using R5T.Richmond;
+
+
+namespace R5T.Solutas.Standard.Construction
+{
+    public class Startup : StartupBase
+    {
+        public Startup(ILogger<Startup> logger)
+            : base(logger)
+        {
+        }
+
+        protected override void ConfigureServicesBody(IServiceCollection services)
+        {
+            services
+                .AddSingleton<Program>()
+                .AddVisualStudioSolutionFileSerializer()
+                .AddTestingDataDirectoryContentPathsProvider()
+                .AddTemporaryDirectoryFilePathProvider()
+                ;
+        }
+    }
+}
