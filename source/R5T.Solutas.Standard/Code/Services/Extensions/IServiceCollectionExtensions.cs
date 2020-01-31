1728683585e1e20c87ed634ea2ac128d302cb192
MinexAutomation <minex.automation@gmail.com>
2020-01-30 22:48:13 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Solutas.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Solutas.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@
 using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Cambridge.Types;
+using R5T.Dacia;
 using R5T.Megara.TextSerializer;
 using R5T.Stockholm.Default;
 using R5T.Solutas.Megara;
@@ -18,9 +19,9 @@ namespace R5T.Solutas.Standard
         /// </summary>
         public static IServiceCollection AddVisualStudioSolutionFileSerializer(this IServiceCollection services)
         {
-            services
-                .AddSingleton<IVisualStudioSolutionFileSerializer, VisualStudioSolutionFileSerializer>()
-                .AddTextFileSerializer<SolutionFile, VisualStudioSolutionFileTextSerializer>()
+            services.AddMegaraVisualStudioSolutionFileSerializer(
+                services.AddTextFileSerializerAction<SolutionFile>(
+                    services.AddVisualStudioSolutionFileTextSerializerAction()))
                 // Configue stream serializer options to use a byte-order-mark for Visual Studio solution files.
                 .Configure<StreamSerializerOptions<SolutionFile>>(options =>
                 {
@@ -29,6 +30,15 @@ namespace R5T.Solutas.Standard
                 ;
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds an <see cref="IVisualStudioSolutionFileSerializer"/> service.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFileSerializer> AddVisualStudioSolutionFileSerializerAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileSerializer>(() => services.AddVisualStudioSolutionFileSerializer());
+            return serviceAction;
         }
     }
 }
